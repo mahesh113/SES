80eff441422990a8fa837fd067fb3fb4746653a4
mahesh113 <mahesh.verma1983@gmail.com>
2024-12-03 00:55:10 +1100

    error fixed. conversion applied

--- SimpleExchaneService/Application/ConversionService.cs
+++ SimpleExchaneService/Application/ConversionService.cs
@@ -1,6 +1,8 @@
-﻿using SimpleExchangeService.Application.Interface;
+﻿using Newtonsoft.Json;
+using SimpleExchangeService.Application.Interface;
 using SimpleExchangeService.Messages.Requests;
 using SimpleExchangeService.Messages.Response;
+using System.Reflection;
 
 namespace SimpleExchangeService.Application
 {
@@ -17,14 +19,31 @@ namespace SimpleExchangeService.Application
         {
             var response = await _httpClient.GetAsync(message.InputCurrency);
             response.EnsureSuccessStatusCode();
+            var respStr = await response.Content.ReadAsStringAsync();
+            var responseObj = JsonConvert.DeserializeObject<ApiResponse>(respStr);
+
+            var rateValue = GetRateValue(responseObj.Rates, message.OutputCurrency);
 
             return new ExchangeResponse
             {
                 Amount = message.Amount,
                 InputCurrency = message.InputCurrency,
                 OutputCurrency = message.OutputCurrency,
-                Value = 2.2m
+                Value = System.Convert.ToDecimal(rateValue) * message.Amount
             };
+        }
+
+        private static object GetRateValue(Rates rates, string outputCurrency)
+        {
+            PropertyInfo[] propInfos = typeof(Rates).GetProperties();
+            foreach (var x in propInfos )
+            {
+                if ( x.Name == outputCurrency)
+                {
+                    return x.GetValue(rates);
+                }
+            }
+            return 0;
         }
     }
 }
---
+++ SimpleExchaneService/Messages/Response/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleExchangeService.Messages.Response
+{
+    public class ApiResponse
+    {
+        public string Result { get; set; }
+        public string Provider { get; set; }
+        public Rates Rates { get; set; }
+    }
+    public class Rates
+    {
+        public string AUD { get; set; }
+        public string USD { get; set; }
+        public string ARS { get; set; }
+        public string CAD { get; set; }
+        public string CHF { get; set; }
+        public string CLP { get; set; }
+        public string EUR { get; set; }
+        public string GBP { get; set; }
+    }
+}
